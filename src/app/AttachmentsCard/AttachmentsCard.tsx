30b106c979e04377bce3e71bcbc3f14b6316d2e6
Ldalvik <subaruforge@gmail.com>
2023-07-06 01:31:35 -0400

    Refactor

--- src/app/AttachmentsCard/AttachmentsCard.tsx
+++ src/app/AttachmentsCard/AttachmentsCard.tsx
@@ -1,15 +1,21 @@
-import { ChangeEvent, MouseEvent, useState } from "react"
-import { Ammo, AttachmentTypeNames, AttachmentTypes, Barrel, Chamber, Magazine, Miscellaneous, Optic, Siderail, Stock, Underbarrel, Weapon } from "../../insurgencyapi/Types"
+import { ChangeEvent, useState } from "react"
+import { Ammo, AttachmentTypeNames, Barrel, Chamber, Magazine, Miscellaneous, Optic, Siderail, Stock, Underbarrel, Weapon } from "../../insurgencyapi/Types"
 import weaponAttachments from "../../insurgencyapi/versus/attachments.json"
 import { IWeaponAttachments } from "../../insurgencyapi/Interfaces"
 
-const AttachmentsPage = ({ weapon }: { weapon: Weapon }) => {
+interface IAttachmentsPage {
+    weapon: Weapon
+}
+
+const AttachmentsPage = ({ weapon }: IAttachmentsPage) => {
+    const ATTACHMENTS = weaponAttachments[weapon]
+
     const [currentAttachments, setCurrentAttachments] = useState<IWeaponAttachments>({
         optic: "NONE" as Optic,
         barrel: "NONE" as Barrel,
         underbarrel: "NONE" as Underbarrel,
-        magazine:"NONE" as Magazine,
-        siderail:"NONE" as Siderail,
+        magazine: "NONE" as Magazine,
+        siderail: "NONE" as Siderail,
         ammo: "NONE" as Ammo,
         stock: "NONE" as Stock,
         miscellaneous: "NONE" as Miscellaneous,
@@ -17,72 +23,81 @@ const AttachmentsPage = ({ weapon }: { weapon: Weapon }) => {
     })
 
     const handleAttachmentChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setCurrentAttachments({
-            ...currentAttachments,
-            [e.currentTarget.id]: e.currentTarget.value
-        })
+        setCurrentAttachments((prevAtt) => ({
+            ...prevAtt,
+            [e.target.id]: e.target.value
+        }))
     }
 
     const handleRandomAttachments = () => {
-        const att = weaponAttachments[weapon]
+        const randomAttachment = (att: AttachmentTypeNames) => ATTACHMENTS[att][Math.floor(Math.random() * ATTACHMENTS[att].length)]
         setCurrentAttachments({
-            optic: att.optic[Math.floor(Math.random() * att.optic.length)] as Optic,
-            barrel: att.barrel[Math.floor(Math.random() * att.barrel.length)] as Barrel,
-            underbarrel: att.underbarrel[Math.floor(Math.random() * att.underbarrel.length)] as Underbarrel,
-            magazine: att.magazine[Math.floor(Math.random() * att.magazine.length)] as Magazine,
-            siderail: att.siderail[Math.floor(Math.random() * att.siderail.length)] as Siderail,
-            ammo: att.ammo[Math.floor(Math.random() * att.ammo.length)] as Ammo,
-            stock: att.stock[Math.floor(Math.random() * att.stock.length)] as Stock,
-            miscellaneous: att.miscellaneous[Math.floor(Math.random() * att.miscellaneous.length)] as Miscellaneous,
-            chamber: att.chamber[Math.floor(Math.random() * att.chamber.length)] as Chamber
+            optic: randomAttachment("optic") as Optic,
+            barrel: randomAttachment("barrel") as Barrel,
+            underbarrel: randomAttachment("underbarrel") as Underbarrel,
+            magazine: randomAttachment("magazine") as Magazine,
+            siderail: randomAttachment("siderail") as Siderail,
+            ammo: randomAttachment("ammo") as Ammo,
+            stock: randomAttachment("stock") as Stock,
+            miscellaneous: randomAttachment("miscellaneous") as Miscellaneous,
+            chamber: randomAttachment("chamber") as Chamber
         })
     }
 
-
     const attachmentSelect = (attachment: AttachmentTypeNames) => {
-        const attachments = weaponAttachments[weapon][attachment]
-        let attachmentMenu = <>
-            <label className="attachments-text">
-                {`${attachment.charAt(0).toUpperCase()}${attachment.slice(1)}`}
-            </label>
-            <select id={attachment} value={currentAttachments[attachment]} className="options-dropdown attachments-dropdown" onChange={handleAttachmentChange}>
-                {attachments.map(name =>
-                    <option value={name} className="text-center" key={name}>
-                        {name}
-                    </option>
-                )}
-            </select>
-        </>
-        if (attachments.length === 0) attachmentMenu = <></>
-        return attachmentMenu
+        return ATTACHMENTS[attachment].length > 0 && (
+            <div className="small-6 attachment-item-section">
+                <label className="attachments-text">
+                    {`${attachment.charAt(0).toUpperCase()}${attachment.slice(1)}`}
+                </label>
+                <select
+                    id={attachment}
+                    value={currentAttachments[attachment]}
+                    className="options-dropdown attachments-dropdown"
+                    onChange={handleAttachmentChange}>
+                    {ATTACHMENTS[attachment].map((name) => (
+                        <option value={name} className="text-center" key={name}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        )
     }
 
     return (
         <div className="card page-card">
             <div className="card-section">
                 <div className="text-center">
-                    <h4>Attachments</h4>
-                    <button onClick={handleRandomAttachments} className="button btn">Randomize attachments</button>
-
+                    {Object.values(ATTACHMENTS).every((array) => array.length === 0) ? (
+                        <>
+                            <h4>The attachment for this weapon hasn't been added! (yet)</h4>
+                            <a href="https://github.com/Ldalvik/insurgency-loadout-generator">
+                                How about you try adding them yourself?
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <h4>Attachments</h4>
+                            <button onClick={handleRandomAttachments} className="button btn">
+                                Randomize attachments
+                            </button>
+                        </>
+                    )}
                 </div>
                 <p>
                     {/* set supply point limit option */}
                 </p>
                 <div className="grid-x">
-                    <div className="small-6 attachments-section">
-                        {attachmentSelect("optic")}
-                        {attachmentSelect("barrel")}
-                        {attachmentSelect("underbarrel")}
-                        {attachmentSelect("magazine")}
-                        {attachmentSelect("siderail")}
-                    </div>
-
-                    <div className="small-6 attachments-section">
-                        {attachmentSelect("ammo")}
-                        {attachmentSelect("stock")}
-                        {attachmentSelect("miscellaneous")}
-                        {attachmentSelect("chamber")}
-                    </div>
+                    {attachmentSelect("optic")}
+                    {attachmentSelect("barrel")}
+                    {attachmentSelect("underbarrel")}
+                    {attachmentSelect("magazine")}
+                    {attachmentSelect("siderail")}
+                    {attachmentSelect("ammo")}
+                    {attachmentSelect("stock")}
+                    {attachmentSelect("miscellaneous")}
+                    {attachmentSelect("chamber")}
                 </div>
             </div>
         </div>
